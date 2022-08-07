69bcf0ff6e409e74b7a236f5b3e074d4ae6dea3e
goosegusgo <goosegusgo@proton.me>
2022-08-07 16:41:19 +0300

    3

--- creation запросы.sql
+++ creation запросы.sql
@@ -1,24 +1,24 @@
 CREATE TABLE IF NOT EXISTS genre (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS artist (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS album (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
-	yearalbum INTEGER NOT NULL,
+	yearalbum INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS track (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	duration INTEGER NOT NULL,
-	album_id INTEGER NOT NULL REFERENCES album(id),
+	album_id INTEGER NOT NULL REFERENCES album(id)
 );
 
 CREATE TABLE IF NOT EXISTS genreartist (
@@ -30,17 +30,17 @@ CREATE TABLE IF NOT EXISTS genreartist (
 CREATE TABLE IF NOT EXISTS albumartist (
 	album_id INTEGER REFERENCES album(id),
 	artist_id INTEGER REFERENCES artist(id),
-	CONSTRAINT pk PRIMARY KEY (album_id , artist_id)
+	CONSTRAINT aa_pk PRIMARY KEY (album_id , artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS collection (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
-	yearcollection INTEGER NOT NULL,
+	yearcollection INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS trackcollections (
 	track_id INTEGER REFERENCES track(id),
 	collection_id INTEGER REFERENCES collection(id),
-	CONSTRAINT pk PRIMARY KEY (track_id , collection_id)
+	CONSTRAINT tc_pk PRIMARY KEY (track_id , collection_id)
 );
